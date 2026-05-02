bde0aaf1df861614537f1efdbdf62ae622510a83
Dimitrije Milenkovic <dimi@Midnight.local>
2026-05-02 18:40:01 +0200

    Initial commit

---
+++ src/main/java/rs/ac/metropolitan/subtrack/SubtrackApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.metropolitan.subtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubtrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubtrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rs/ac/metropolitan/subtrack/SubtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.metropolitan.subtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubtrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
